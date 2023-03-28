42633147311255641edce227e1629655b4ab71d8
brandonall <Brandonallgood007@gmail.com>
2023-03-28 13:47:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ var UserStatus;
 })(UserStatus || (UserStatus = {}));
 var Default;
 (function (Default) {
-    Default["PIN"] = "1234";
+    Default["PIN"] = "9959";
 })(Default || (Default = {}));
 var WeatherType;
 (function (WeatherType) {
